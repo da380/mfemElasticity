3afd74fbe7b5b5b8773603a9147ca64d74257a64
Ziheng <sssou@Solver2023.localdomain>
2026-04-20 15:51:33 +0100

    .

--- installed/include/mfemElasticity.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "mfem.hpp"
-#include "mfemElasticity/bilininteg.hpp"
-#include "mfemElasticity/legendre.hpp"
-#include "mfemElasticity/lininteg.hpp"
-#include "mfemElasticity/mesh.hpp"
-#include "mfemElasticity/poisson.hpp"
-#include "mfemElasticity/solvers.hpp"
